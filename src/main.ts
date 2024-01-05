99e02f81e0f4c5a6c783a3d2b84081982de00a67
Robert Harris <github.com@trebor.org>
2024-01-05 12:13:19 -0800

    permit dashes (#7)
    * permit dashes in project name
    
    * update error

--- src/main.ts
+++ src/main.ts
@@ -90,8 +90,8 @@ function validateProjectName(projectDir: string, projectName: string): string | 
     return "Project name must be at least 1 character long.";
   }
 
-  if (!/^([^0-9\W]\w*)$/.test(projectName)) {
-    return "Project name must contain only alphanumerics or underscore with no leading digits.";
+  if (!/^([^0-9\W][\w-]*)$/.test(projectName)) {
+    return "Project name must contain only alphanumerics, dash or underscore with no leading digits.";
   }
 
   return true;
